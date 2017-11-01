876ab824f00bcb34bf394c3224e5ab26054a9045
zhengchun <zhengchunster@gmail.com>
2017-11-01 17:22:52 +0800

    move tokenize() to __init__.py

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from tokenizer import MMSegTokenizer
+
+
+class _mmsegLazy():
+    def __init__(self):
+        self.tokenizer = None
+
+    def __call__(self):
+        if self.tokenizer is None:
+            self.tokenizer = MMSegTokenizer()
+        return self.tokenizer
+
+
+_default = _mmsegLazy()
+
+
+def tokenize(doc):
+    return _default().tokenize(doc)
